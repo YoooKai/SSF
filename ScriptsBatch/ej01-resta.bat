57cdb31c801b17a945cd30cee6eaeb4de7f95bf7
Kai <145053705+YoooKai@users.noreply.github.com>
2024-02-19 22:18:28 +0000

    Add files via upload

---
+++ ScriptsBatch/ej01-resta.bat
@@ -0,0 +1,20 @@
+@echo off
+
+title Resta
+
+REM ********************************
+REM * Nombre: resta.bat
+REM * Autor: Kai Rodríguez García
+REM *
+REM * Objetivo: Restar dos valores
+REM * 
+REM *
+REM ********************************
+
+set /a a = 35
+set /a b = 11
+
+set /a resultado = %a% + %b%
+
+echo La resta de %a% + %b% = %resultado%
+
